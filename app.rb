664c97de8c2f26f64419550f38c2aa056b3b0a7c
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-09 18:41:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,11 @@ class App < Sinatra::Base
   get "/" do
     erb :user_input
   end
+
+  post "/" do
+    @user_phrase = params[:user_phrase]
+    phrase = PigLatinizer.new
+    phrase.to_pig_latin(@user_phrase)
+    erb :user_input
+  end
 end
